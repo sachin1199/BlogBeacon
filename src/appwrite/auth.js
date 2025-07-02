e156ac180a19bf789d33ab954f258da9225bccbb
sachin1199 <sachinsingh94070@gmail.com>
2025-07-02 16:03:21 +0530

    changed the login function to delete the logged in session first

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -2,8 +2,8 @@ import { Client, Account, ID } from 'appwrite'
 import conf from '../conf/conf'
 
 class AuthService {
-  client = new Client();
-  account;
+  client = new Client()
+  account
   constructor() {
     this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProjectId)
     this.account = new Account(this.client)
@@ -12,69 +12,56 @@ class AuthService {
   // creating the signup functionality
 
   async createAccount({ email, password, name }) {
-    
     try {
       const userAccount = await this.account.create(
         ID.unique(),
         email,
         password,
         name
-      );
+      )
       if (userAccount) {
-          return this.login({ email, password });
+        return this.login({ email, password })
       } else {
-        return userAccount;
+        return userAccount
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
       throw error
     }
+  }
+
+  // creating the login functionality
+  async login({ email, password }) {
+    try {
+      // Logout first to avoid "session already exists" error
+      await this.account.deleteSessions().catch(() => {})
+
+      return await this.account.createEmailPasswordSession(email, password)
+    } catch (error) {
+      console.log(error)
+      throw error
     }
+  }
 
-    // creating the login functionality
-    async login({ email, password }) {
-        try {
-            return await this.account.createEmailPasswordSession(
-                email,
-                password
-            )
-            
-        } catch (error) {
-            console.log(error);
-            throw error;
-
-
-            
-        }
+  // getting the current user if still logged in visits
+  async getCurrentUser() {
+    try {
+      return await this.account.get()
+    } catch (error) {
+      console.log(error)
+      throw error
     }
-    // getting the current user if still logged in visits
-    async getCurrentUser() {
-         try {
-             return await this.account.get();
-             
-         } catch (error) {
-             console.log(error);
-             throw error;
+  }
 
-
-            
-        }
+  // create logout functionality
+  async logout() {
+    try {
+      return await this.account.deleteSessions()
+    } catch (error) {
+      console.log(error)
+      throw error
     }
-    
-
-    // create logout functionality
-    async logout() {
-        try {
-            return await this.account.deleteSessions();
-
-            
-        } catch (error) {
-            console.log(error);
-            throw error;
-
-        }
-    }
-
+  }
 }
 
 const authService = new AuthService()
