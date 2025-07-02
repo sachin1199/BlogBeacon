ff7375a04e36dfd5daca95aba94090e7c7ac0389
sachin1199 <sachinsingh94070@gmail.com>
2025-07-02 15:51:22 +0530

    changed the app component and removed the get current user

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import authService from './appwrite/auth'
-import { loginStore, logoutStore } from './store/authSlice'
-import { useDispatch } from 'react-redux'
+import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const dispatch=useDispatch()
-
-
-  useEffect(() => {
-    authService.getCurrentUser().then((userData) => {
-      if (userData) {
-          dispatch(loginStore({ userData }))
-
-      }
-      else {
-        dispatch(logoutStore())
-      }
-}).finally(()=>setLoading(false))
-    
-  },[])
-  
-  
-  return !loading ? (
+  return (
     <div className="min-h-screen flex flex-wrap content-between bg-gradient-to-r from-purple-600 via-indigo-600 to-blue-600 shadow-lg">
       <div className="w-full block">
         <Header />
@@ -33,8 +12,7 @@ function App() {
         <Footer />
       </div>
     </div>
-  ) : null
-  
+  )
 }
 
 export default App
